d9f89201a4804af3688a58c5d993e61997db3316
ahulterstrom <andrewhulterstrom@gmail.com>
2019-11-08 15:51:15 +0000

    finalized site

--- public/javascripts/vue.js
+++ public/javascripts/vue.js
@@ -3,22 +3,108 @@ var app = new Vue({
     data: {
         posts: [],
         userposttext: "",
-        username: "Andrew",
+        username: "",
+        createaccount: false,
+        login: false,
+        loggedin: false,
+        accountcreated: false,
+        waiting: false,
+        usernameinput: '',
+        passwordinput: '',
     },
     methods: {
+        createorlog(){
+            this.accountcreated = false;
+            this.login = false;
+            this.createaccount = false;
+        },
+        showcreateaccount(){
+            this.accountcreated = false;
+            this.login = false;
+            this.createaccount = true;
+        },
+        showlogin(){
+            this.accountcreated = false;
+            this.createaccount = false;
+            this.login = true;
+        },
+        async createAccount() {
+            if(this.usernameinput == "" || this.passwordinput == ""){
+                alert("Please enter both a Username and Password");
+                return;
+            }
+            console.log("creating account");
+            var url = "http://cs260.andrewhulterstrom.com:3001/newuser";
+            try{
+                var response = await axios.post(url,{
+                    username: this.usernameinput,
+                    password: this.passwordinput,
+                });
+                if(response.data){
+                    this.usernameinput="";
+                    this.passwordinput="";
+                    this.accountcreated = true;
+                    this.createaccount = false;
+                }
+                else{
+                    alert("Sorry, that Username is already taken");
+                }
+            }
+            catch(err){
+                console.log("error");
+            }
+        },
+        async checkCredentials() {
+            if(this.usernameinput == "" || this.passwordinput == ""){
+                alert("Please enter both a Username and Password");
+                return;
+            }
+            console.log("attempting to log in...");
+            var url = "http://cs260.andrewhulterstrom.com:3001/login";
+            var name = this.usernameinput;
+            var pass = this.passwordinput;
+            var user={name:name,pass:pass};
+            try{
+                console.log(name);
+                console.log(pass);
+                var response = await axios.post(url,{
+                    username: name,
+                    password: pass
+                });
+                if(response.data){
+                    //Log in success!
+                    this.getPosts();
+                    let timerId = setInterval(() => this.getPosts(), 5000);
+                    setTimeout(() => { clearInterval(timerId); }, 1000000);
+                    this.username = name;
+                    this.loggedin = true;
+                }
+                else{
+                    alert("Invalid Username and Password");
+                }
+            }
+            catch(err){
+                console.log("error");
+            }
+        },
         async getPosts() {
+            if(this.waiting == true){
+                console.log("Still waiting for response...");
+                return;
+            }
             console.log("requesting posts...");
+            this.waiting = true;
             var url = "http://cs260.andrewhulterstrom.com:3001/posts";
             try {
                 var response = await axios.get(url);
                 var data = response.data;
                 this.posts = data;
-                
                 this.setRelativeTimes();
             }
             catch (err) {
                 console.log("Could not get posts");
             }
+            this.waiting = false;
         },
         async makeuserpost() {
             var text = this.userposttext;
@@ -47,7 +133,10 @@ var app = new Vue({
         }
     },
     computed: {
-
+        remainingchars(){
+            var length = this.userposttext.length;
+            return (28 - length);
+        }
     },
     created() {
         this.getPosts();
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var posts = [];
+var users = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,10 +21,60 @@ router.post('/submituserpost', function(req,res){
     posts.push(userpost);
     res.send('Adding your post');
     return;
-})
+});
 
+router.post('/newuser', function(req,res){
+    console.log("new user request");
+    console.log(req.body);
+    var newuser = req.body;
+    if(usernameistaken(newuser.username)){
+        res.send(false);
+    }
+    else{
+        users.push(newuser);
+        res.send(true);
+    }
+    return;
+});
 
+router.post('/login', function(req,res){
+    console.log("log in request");
+    console.log(req.body);
+    var user = req.body;
+    if(usernamepasswordmatch(user)){
+        res.send(true);
+    }
+    else{
+        res.send(false);
+    }
+    return;
+});
 
 
 
 module.exports = router;
+
+
+
+function usernameistaken(name){
+    for(var i=0;i<users.length;i++){
+        if(users[i].username == name){
+            return true;
+        }
+    }
+    return false;
+}
+
+function usernamepasswordmatch(user){
+    for(var i=0;i<users.length;i++){
+        if(users[i].username == user.username){
+            if(users[i].password == user.password){
+                return true;
+            }
+            else{
+                return false; //wrong password
+            }
+        }
+    }
+    return false; //wrong username
+}
